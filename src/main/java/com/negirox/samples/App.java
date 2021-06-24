d5e6a2f8959dd616601066817c04743609b13bd8
Negirox <mukeshsingh.negi007@gmail.com>
2021-06-24 10:05:45 +0530

    first commit

---
+++ src/main/java/com/negirox/samples/App.java
@@ -0,0 +1,20 @@
+package com.negirox.samples;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.html2pdf.HtmlConverter;
+
+public class App 
+{
+	
+    public static void main( String[] args ) throws FileNotFoundException, IOException  
+    {
+    	// HTML file to PDF
+    	HtmlConverter.convertToPdf(new FileInputStream("index.html"), 
+    			new FileOutputStream("PDFwithHTMLandCSS.pdf"));
+        System.out.println( "PDF Created!" );
+    }
+}
